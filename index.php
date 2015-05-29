9c4ac9d99fba1e67a8ecd32a38afd8889683ee59
Sarah <sarah.heindryckx@hotmail.com>
2015-05-29 13:15:41 +0200

    message

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link href="style.css" rel="stylesheet" >
+        <title></title>
+    </head>
+    <body>
+        <?php
+        echo "hello";
+        ?>
+        <div><img src="img/img.jpg" ></div>
+    </body>
+</html>
